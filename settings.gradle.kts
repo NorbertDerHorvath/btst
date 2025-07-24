280c7abdaa787efe5f874dc57486516863986071
NorbertDerHorvath <horvath-norbert@web.de>
2025-07-24 20:55:14 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,12 +4,14 @@ pluginManagement {
         google()
         mavenCentral()
     }
-   plugins {
-    alias(libs.plugins.`android-gradle-plugin`)
-    alias(libs.plugins.`kotlin-android`)
-    alias(libs.plugins.`kotlin-compose`)
-    alias(libs.plugins.`kotlin-kapt`)
-}
+    // Itt NE használj alias-t!
+    // Ha nagyon szükséges, így adj meg plugint:
+    // plugins {
+    //     id("com.android.application") version "8.4.0"
+    //     id("org.jetbrains.kotlin.android") version "2.0.21"
+    //     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    //     id("org.jetbrains.kotlin.kapt") version "2.0.21"
+    // }
 }
 
 dependencyResolutionManagement {
@@ -18,8 +20,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-
-    // register the TOML for dependencies
     versionCatalogs {
         create("libs") {
             from(files("gradle/libs.versions.toml"))
